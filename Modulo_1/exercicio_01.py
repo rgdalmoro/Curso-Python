711b484605624adbfad101be2376352507c52f11
Rafael Dal Moro <rafaelgdalmoro@gmail.com>
2026-05-31 16:38:06 -0300

    exercicio 1 e 2

---
+++ Modulo_1/exercicio_01.py
@@ -0,0 +1,5 @@
+#Exercicio 1
+print('Ola Mundo!')
+
+msg = 'Ola Mundo!'
+print(msg)
---
+++ Modulo_1/exercicio_02.py
@@ -0,0 +1,4 @@
+#Exercicio 2
+nome=str(input('Digite seu nome:'))
+print('Bom dia,',nome,'! Tudo bem com você?')
+print('Bom dia, {}! Tudo bem com você?'.format(nome))
